3e57fb292bf3b7a34c272b0128a1ae6091be5efc
Gehan Anwer <amrsheta751@gmail.com>
2024-06-11 17:42:33 +0300

    Remove unnecessary dependency and update prediction logic

--- priceClassificationSystem/src/main/java/com/device_classifier/services/DeviceService.java
+++ priceClassificationSystem/src/main/java/com/device_classifier/services/DeviceService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @Service
 public class DeviceService {
@@ -28,15 +30,20 @@ public class DeviceService {
     public String predict(int deviceId) {
         final String uri = "http://127.0.0.1:5000/predict";
         Device toBePredicted = getDeviceById(deviceId);
-        //Gson gson = new Gson();
-        //String json = gson.toJson(toBePredicted);
         RestTemplate restTemplate = new RestTemplate();
-        
         String response = restTemplate.postForObject(uri, toBePredicted, String.class);
-        int predictionNumber = Integer.parseInt(response.substring(18,19));
-        // update device 
-        toBePredicted.setPrice_range(predictionNumber);
-        deviceRepository.save(toBePredicted);
-        return "Prediction result for device with ID: " + response.substring(18,19);
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            JsonNode jsonNode = mapper.readTree(response);
+            JsonNode predictionNode = jsonNode.path("prediction");
+            String prediction = predictionNode.asText();
+            int predictionNumber = Integer.parseInt(prediction);
+            toBePredicted.setPrice_range(predictionNumber);
+            deviceRepository.save(toBePredicted);
+            return "Prediction result for device with ID: " + predictionNumber;
+        } catch(Exception e){
+            return "Error: " + e.getMessage();
+        }
+
     }
 }
